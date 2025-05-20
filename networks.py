0e10bd73843af454b9aeb3e0aac2d67e32e7be2a
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-05-19 20:16:12 -0600

    Add neural network structure

---
+++ networks.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+import torch
+
+class ActorNetwork(nn.Module):
+    def __init__(self, num_thrusters):
+        self.stack = nn.Sequential(
+            nn.Linear(18 + num_thrusters, 600),
+            nn.ReLU(),
+            nn.Linear(600, 400),
+            nn.ReLU(),
+            nn.Linear(400, 300),
+            nn.ReLU(),
+            nn.Linear(300, num_thrusters),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.stack(x)
+
+class CriticNetwork(nn.Module):
+    def __init__(self, num_thrusters):
+        self.num_thrusters = num_thrusters
+        self.linear = nn.Linear(18, 600)
+        self.stack = nn.Sequential(
+            nn.Linear(600 + num_thrusters, 400),
+            nn.ReLU(),
+            nn.Linear(400, 300),
+            nn.ReLU(),
+            nn.Linear(300, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        out = self.linear(x[:self.num_thrusters])
+        return self.stack(torch.cat(out, x[self.num_thrusters:]))
