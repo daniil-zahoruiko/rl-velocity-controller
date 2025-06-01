abafddf7c2234f8b35392a925de9cf15d8a6e6c6
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-05-31 23:38:44 -0600

    Add inference time to testing script

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from stable_baselines3 import SAC
 import gymnasium as gym
 from gymnasium.envs.registration import register
+import time
 
 register(
     id="pool_train",
@@ -15,12 +16,15 @@ env   = gym.make("pool_train")
 
 n_episodes = 10
 rewards = []
+inference_times = []
 for ep in range(n_episodes):
     obs, _ = env.reset()
     done = False
     ep_reward = 0.0
     while not done:
+        start_time = time.time()
         action, _ = model.predict(obs, deterministic=True)
+        inference_times.append(time.time() - start_time)
         obs, r, terminated, truncated, _ = env.step(action)
         done = terminated or truncated
         ep_reward += r
@@ -30,4 +34,5 @@ for ep in range(n_episodes):
         env.unwrapped.plot_last_episode(f'_test_{ep}')
 
 print(f"\nMean reward over {n_episodes} eps = {sum(rewards)/n_episodes:.3f}")
+print(f"\nAverage inference time: {sum(inference_times) / len(inference_times)}")
 env.close()
