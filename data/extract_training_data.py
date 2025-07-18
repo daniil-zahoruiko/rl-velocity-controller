ef1dbaaaf8828c9e0e2d7e51c8a0d811b8ac8a93
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-07-17 19:07:18 -0600

    Implement topic id mapping

--- data/extract_training_data.py
+++ data/extract_training_data.py
@@ -1,6 +1,16 @@
 import sqlite3
 import argparse
 
+TOPIC_NAMES = ['/dynamics/state', '/dynamics/target', '/thrusters/target_thrust']
+
+def get_topic_id_mapping(cursor: sqlite3.Cursor):
+    res = cursor.execute('SELECT id, name FROM topics WHERE name IN (%s)' % ','.join('?' * len(TOPIC_NAMES)), TOPIC_NAMES)
+    id_mapping = {}
+    for (id, name) in res.fetchall():
+        id_mapping[name] = id
+
+    return id_mapping
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('db_path', nargs=1)
@@ -10,6 +20,7 @@ def main():
     db_path = args.db_path[0]
     con = sqlite3.connect(db_path)
     cur = con.cursor()
+    print(get_topic_id_mapping(cur))
 
 if __name__ == '__main__':
     main()
