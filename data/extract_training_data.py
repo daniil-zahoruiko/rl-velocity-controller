cac6c3081bf1f79a16bb5bde72cb1bc525fc01d4
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-07-17 19:47:51 -0600

    Switch to using rosbag2 api

--- data/extract_training_data.py
+++ data/extract_training_data.py
@@ -1,36 +1,30 @@
-import sqlite3
 import argparse
 
-TOPIC_NAMES = ['/dynamics/state', '/dynamics/target', '/thrusters/target_thrust']
+import rosbag2_py
 
-def get_topic_id_mapping(cursor: sqlite3.Cursor):
-    res = cursor.execute('SELECT id, name FROM topics WHERE name IN (%s)' % ','.join('?' * len(TOPIC_NAMES)), TOPIC_NAMES)
-    id_mapping = {}
-    for (id, name) in res.fetchall():
-        id_mapping[name] = id
+TOPIC_NAMES = ["/dynamics/state", "/dynamics/target", "/thrusters/target_thrust"]
 
-    return id_mapping
-
-def get_messages(cursor: sqlite3.Cursor, topic_id):
-    res = cursor.execute('SELECT timestamp, data FROM messages WHERE topic_id=? LIMIT 10', (topic_id, )) # NOTE: LIMIT only for testing purposes
-    return res.fetchall()
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('db_path', nargs=1)
+    argparser.add_argument("bag_path", nargs=1)
 
     args = argparser.parse_args()
+    bag_path = args.bag_path[0]
 
-    db_path = args.db_path[0]
-    con = sqlite3.connect(db_path)
-    cur = con.cursor()
-    id_mapping = get_topic_id_mapping(cur)
-    states = get_messages(cur, id_mapping['/dynamics/state'])
-    targets = get_messages(cur, id_mapping['/dynamics/target'])
-    actions = get_messages(cur, id_mapping['/thrusters/target_thrust'])
-    print(states)
-    print(targets)
-    print(actions)
+    reader = rosbag2_py.SequentialReader()
+    reader.open(
+        rosbag2_py.StorageOptions(
+            uri=bag_path,
+            storage_id="sqlite3",
+        ),
+        rosbag2_py.ConverterOptions("", "cdr"),
+    )
 
-if __name__ == '__main__':
+    topic_types = reader.get_all_topics_and_types()
+    type_map = {topic.name: topic.type for topic in topic_types}
+    print(type_map)
+
+
+if __name__ == "__main__":
     main()
