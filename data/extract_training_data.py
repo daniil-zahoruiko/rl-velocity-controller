2a2157a8ee5bda93f41d4a600f54ae16432c6f8a
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-07-17 19:20:22 -0600

    Retrieve messages

--- data/extract_training_data.py
+++ data/extract_training_data.py
@@ -11,6 +11,10 @@ def get_topic_id_mapping(cursor: sqlite3.Cursor):
 
     return id_mapping
 
+def get_messages(cursor: sqlite3.Cursor, topic_id):
+    res = cursor.execute('SELECT timestamp, data FROM messages WHERE topic_id=? LIMIT 10', (topic_id, )) # NOTE: LIMIT only for testing purposes
+    return res.fetchall()
+
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('db_path', nargs=1)
@@ -20,7 +24,13 @@ def main():
     db_path = args.db_path[0]
     con = sqlite3.connect(db_path)
     cur = con.cursor()
-    print(get_topic_id_mapping(cur))
+    id_mapping = get_topic_id_mapping(cur)
+    states = get_messages(cur, id_mapping['/dynamics/state'])
+    targets = get_messages(cur, id_mapping['/dynamics/target'])
+    actions = get_messages(cur, id_mapping['/thrusters/target_thrust'])
+    print(states)
+    print(targets)
+    print(actions)
 
 if __name__ == '__main__':
     main()
